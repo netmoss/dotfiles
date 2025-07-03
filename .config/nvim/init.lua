76e291de066c50ebe43da84aa8b6502bd5627ec8
netmoss <64058049+netmoss@users.noreply.github.com>
2025-07-04 02:16:22 +1000

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -158,6 +158,9 @@ require("lazy").setup({
             end,
         },
         {
+            "neovim/nvim-lspconfig",
+        },
+        {
             "hrsh7th/nvim-cmp",
             dependencies = {
                 "hrsh7th/cmp-nvim-lsp",
@@ -198,13 +201,6 @@ require("lazy").setup({
                     end,
                 })
             end
-        },
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-            ---@module 'render-markdown'
-            ---@type render.md.UserConfig
-            opts = {},
         },
     },
     install = { colorscheme = { "github_dark_default" } },
@@ -265,6 +261,7 @@ local makeprg_map = {
     arduino =
     "arduino-cli compile --fqbn arduino:avr:uno % && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno %",
     java = "javac % && java %:r",
+    sh = "sh %",
 }
 
 -- function to set makeprg
@@ -303,27 +300,25 @@ require("lspconfig").rust_analyzer.setup({})
 require("lspconfig").clangd.setup({})
 require("lspconfig").arduino_language_server.setup({})
 
----- binds
+-- binds
 -- neotree
-vim.keymap.set("n", "\\", ":Neotree toggle<CR>", { noremap = true, silent = true, desc = "Toggle Neotree" })
+vim.keymap.set("n", "\\", ":Neotree toggle<CR>", { desc = "Toggle Neotree" })
 
 -- fuzzy
-vim.keymap.set("n", "<leader>ff", ":Files<CR>", { noremap = true, desc = "Fuzzy find files" })
-vim.keymap.set("n", "<leader>fg", ":Rg<CR>", { noremap = true, desc = "Fuzzy live grep" })
-vim.keymap.set("n", "<leader>fb", ":Buffers<CR>", { noremap = true, desc = "Fuzzy buffers" })
-vim.keymap.set("n", "<leader>fh", ":Helptags<CR>", { noremap = true, desc = "Fuzzy help tags" })
+vim.keymap.set("n", "<leader>ff", ":Files<CR>", { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>fg", ":Rg<CR>", { desc = "Fuzzy live grep" })
+vim.keymap.set("n", "<leader>fb", ":Buffers<CR>", { desc = "Fuzzy buffers" })
+vim.keymap.set("n", "<leader>fh", ":Helptags<CR>", { desc = "Fuzzy help tags" })
 
 -- dev
 vim.keymap.set("n", "<leader>d", "<Cmd>lua vim.lsp.buf.hover()<CR>",
     { noremap = true, silent = true, desc = "Display documentation" })
-vim.keymap.set("n", "<leader>t", ":Trouble<CR>", { noremap = true, silent = true, desc = "Toggle Trouble diagnostics" })
+vim.keymap.set("n", "<leader>t", ":Trouble<CR>", { desc = "Toggle Trouble diagnostics" })
 vim.keymap.set("n", "<leader>m", ":TermMake<CR>", { noremap = true, silent = true, desc = "Compile and run" })
 vim.keymap.set("t", "<leader>q", [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Quit terminal buffer" })
-vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, desc = "Write" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, desc = "Quit" })
-vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true, desc = "Write and quit" })
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true, desc = "Next buffer" })
-vim.keymap.set('n', '<leader>bb', ':bprevious<CR>', { noremap = true, silent = true, desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "Write and quit" })
 
 -- windows
 vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", { noremap = true, silent = true, desc = "Vertical split" })
@@ -338,6 +333,8 @@ vim.keymap.set("n", "<C-S-l>", ":vertical resize +2<CR>",
     { noremap = true, silent = true, desc = "Resize vertical split right" })
 vim.keymap.set("n", "<C-S-j>", ":resize +2<CR>", { noremap = true, silent = true, desc = "Resize horizontal split down" })
 vim.keymap.set("n", "<C-S-k>", ":resize -2<CR>", { noremap = true, silent = true, desc = "Resize horizontal split up" })
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bb", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
 
 -- misc
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>", { noremap = true, silent = true })
